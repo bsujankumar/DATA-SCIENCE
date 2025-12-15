d6715b2a2945aa7713a341e7437a01e7f4d8f867
bsujankumar <bsujan.ad24@bmsce.ac.in>
2025-12-15 23:38:46 +0530

    Add files via upload

--- singlyLL.c
+++ singlyLL.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 typedef struct Node{
     int data;
     struct Node *next;
 }Node;
+
 void addFirst(Node **head, int value){
     Node *new_node = (Node*)malloc(sizeof(Node));
     new_node -> data = value;
+    new_node -> next = NULL;
     if(*head == NULL){
       *head = new_node;
       return;
@@ -14,6 +17,7 @@ void addFirst(Node **head, int value){
     new_node -> next = *head;
     *head = new_node;
 }
+
 void addLast(Node **head, int value){
     Node *new_node = (Node*)malloc(sizeof(Node));
     new_node -> data = value;
@@ -28,6 +32,7 @@ void addLast(Node **head, int value){
     }
     temp -> next = new_node;
 }
+
 void addPos(Node **head, int pos, int value){
     if(pos == 1){
         addFirst(head, value);
@@ -52,21 +57,91 @@ void addPos(Node **head, int pos, int value){
     newNode -> next = temp -> next;
     temp -> next = newNode;
 }
-void display(Node **head){
+
+void deleteFirst(Node **head){
+    if(*head == NULL){
+        printf("The Linked list is empty\n");
+        return;
+    }
     Node *temp = *head;
+    *head = (*head) -> next;
+    free(temp);
+}
+
+void deleteLast(Node **head){
+    if(*head == NULL){
+        printf("The Linked list is empty\n");
+    }
+    else if((*head) -> next == NULL){
+        free(*head);
+        *head = NULL;
+    }
+    else{
+        Node *temp = *head;
+        Node *prev = NULL;
+        while(temp -> next != NULL){
+            prev = temp;
+            temp = temp -> next;
+        }
+        prev -> next = NULL;
+        free(temp);
+    }
+}
+
+void display(Node *head){
+    Node *temp = head;
     while(temp!= NULL){
         printf("%d -> ",temp->data);
         temp = temp -> next;
     }
-    printf("NULL");
+    printf("NULL\n");
 }
+
+void itrSearch(Node **head , int key){
+    if(*head == NULL){
+        printf("Linked list is empty\n");
+        return;
+    }
+    Node *temp = *head;
+    while(temp != NULL){
+        if(temp -> data == key){
+            printf("Key is found\n");
+            return;
+        }
+        temp = temp -> next;
+    }
+    printf("Key not found\n");
+}
+
+int sizeOf(Node *head){
+    if(head == NULL){
+        printf("Linked list is empty\n");
+        return 0;
+    }
+    Node *temp = head;
+    int size = 1;
+    while(temp -> next != NULL){
+        size += 1;
+        temp = temp -> next;
+    }
+    return size;
+}
+
 int main(){
     Node *head = NULL;
     addFirst(&head,20);
     addFirst(&head,10);
     addLast(&head,40);
     addLast(&head,50);
-    display(&head);
-
-
+    display(head);
+    deleteFirst(&head);
+    display(head);
+    deleteLast(&head);
+    display(head);
+    deleteFirst(&head);
+    display(head);
+    itrSearch(&head, 80);
+    printf("%d\n",sizeOf(head));
+    deleteLast(&head);
+    display(head);
 }
